9991c2afbde80d830a418eb6d8366916d5b9313d
ASLeonard <alexander.leonard@usys.ethz.ch>
2025-09-25 12:29:06 +0200

    more formatting improvements

--- blocks.lua
+++ blocks.lua
@@ -1,10 +1,12 @@
 if FORMAT:match 'beamer' then
   function Span(el)
+    local blocks = { exampleblock=true, definitionblock=true }
     if el.classes[1] == "alert" then
       table.insert(el.content, 1, pandoc.RawInline("latex", "\\alert{"))
       table.insert(el.content, pandoc.RawInline("latex", "}"))
-    elseif el.classes[1] == "exampleblock" then
-      table.insert(el.content, 1, pandoc.RawInline("latex", "\\begin{"..el.classes[1].."}{}"))
+    elseif blocks[el.classes[1]] then
+      if el.attributes["title"] == nil then el.attributes["title"] = "" end
+      table.insert(el.content, 1, pandoc.RawInline("latex", "\\begin{"..el.classes[1].."}["..el.attributes["title"].."]"))
       table.insert(el.content, pandoc.RawInline("latex", "\\end{"..el.classes[1].."}"))
     end
     return el
